ad28dd05af863ae72988cd9f979df0a996370a8b
haruki <haruki@MSI>
2023-10-30 14:23:13 +0900

    関数の追加

--- main.cpp
+++ main.cpp
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
-int CalculateSalary(int n1 ,int n2, int n3,int count) {
-	int n = 0;
-	
-	if (n1+n3 > n2) {
-		printf("%d時間で再帰的な賃金体系のほうが儲かる",count );
-		return (1);
-	}
-	else {
-		count += 1;
-		
-		if (count > 1) {
-			n3 = n1 + n3;
-			n1 = (n1 * 2 - 50);
-			n2 = n2 + 1072;
-		}
-		
-		printf("再帰自給[%d]給料[%d再帰,%d一般],%d時給\n", n1,n1+n3,n2,count);
-		
-		return CalculateSalary(n1, n2,n3, count);
-	}
-	
+int AddSalary(int value1) {
+	return value1 + 1072;
+}
 
+int CalculateSalary(int value2) {
+	return value2 * 2 - 50;
+}
+
+int CompartionSalary(int value1, int value2,int num ,int count) {
+	count++;
+	
+	printf("再帰自給[%d]給料[%d再帰,%d一般],%d時給\n", value2, num + value2, value1, count);
+
+	value1 = AddSalary(value1);
+	value2 = CalculateSalary(value2);
+	num = num + value2;
+	
+	if (value1 <= value2) {
+	printf("%d時間で再帰的な賃金体系のほうが儲かる",count );
+	return (1);
+    }
+
+	CompartionSalary(value1,value2,num, count);
 	
 }
 
@@ -31,15 +32,17 @@ int CalculateSalary(int n1 ,int n2, int n3,int count) {
 int main() {
 	//カウント
 	int count = 0;
-	//再帰的時給
-	int n = 100;
 	//一般的給料
 	int value1 = 1072;
-	int value2 = 0;
-	int result = 0;
-
-	result = CalculateSalary(n,value1,value2,count);
+	int num = 0;
+	//再帰的時給
+	int value2 = 100;
 	
+	
+
+	CompartionSalary(value1, value2,num,count);
+	
+
 	
 	
 	return 0;
