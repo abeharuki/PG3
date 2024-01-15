6cec898737b689edf8ddf1449adad67d2607f89b
haruki <haruki@MSI>
2024-01-15 11:05:56 +0900

    マルチスレッド

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <iostream>
+#include <thread>
+
+void thread(std::string thread) {
+	std::cout << thread << std::endl;
+}
+
 
 int main() {
 	
+	std::thread th1(thread,"thread1");
+	th1.join();
+
+	std::thread th2(thread, "thread2");
+	th2.join();
+
+	std::thread th3(thread, "thread3");
+	th3.join();
+
+
 	return 0;
 }
